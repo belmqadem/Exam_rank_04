c1ce5fb3f1498f92c77d13c808d9d509932eee4a
valarmo <adilfront3@gmail.com>
2024-10-16 14:48:47 +0100

    modif

--- microshell.c
+++ microshell.c
@@ -35,12 +35,12 @@ int		cd(char **av)
 }
 
 // This function handles the execution of a command and manages piping between processes.
-int		exec_cmd(char **av, char **env, int *fd_in, int has_pipe, int *pipe_fd)
+int		exec_cmd(char **av, char **env, int *infd, int has_pipe, int *pipefd)
 {
 	int pid, status;
-	if (has_pipe && pipe(pipe_fd) == -1)
+	if (has_pipe && pipe(pipefd) == -1)
 	{
-		err("error: fatala\n");
+		err("error: fatal\n");
 		exit (1);
 	}
 	if ((pid = fork()) == -1)
@@ -50,37 +50,34 @@ int		exec_cmd(char **av, char **env, int *fd_in, int has_pipe, int *pipe_fd)
 	}
 	if (pid == 0)
 	{
-		if (fd_in && *fd_in != -1 && dup2(*fd_in, 0) == -1)
+		if (infd && *infd != -1 && dup2(*infd, 0) == -1)
 		{
 			err("error: fatal\n");
 			exit(1);
 		}
-		if (has_pipe && dup2(pipe_fd[1], 1) == -1)
+		if (has_pipe && dup2(pipefd[1], 1) == -1)
 		{
 			err("error: fatal\n");
 			exit (1);
-		}
-		close_fds(fd_in);
-		close_fds(pipe_fd);
+		}	
+		close_fds(infd);
+		close_fds(pipefd);
 		execve(av[0], av, env);
 		err("error: cannot execute ");
         err(av[0]);
         err("\n");
         exit(1);
 	}
-	if (fd_in && *fd_in != -1) close (*fd_in);
-	if (has_pipe) close(pipe_fd[1]);
-
-	if (!has_pipe)
-		waitpid(pid, &status, 0);
+	if (infd && *infd != -1) close (*infd);
+	if (has_pipe) close(pipefd[1]);
+	if (!has_pipe) waitpid(pid, &status, 0);
 	return (pid);
 }
 
 int	main(int ac, char **av, char **env)
 {
-	int i = 1, fd_in = -1, status = 0, has_pipe = 0, pipe_fd[2];
+	int i = 1, status = 0, has_pipe = 0, infd = -1, pipefd[2];
 	char **cmd_start = NULL;
-
 	(void)ac;
 	while (av[i])
 	{
@@ -99,13 +96,13 @@ int	main(int ac, char **av, char **env)
 			}
 			else
 			{
-				exec_cmd(cmd_start, env, &fd_in, has_pipe, pipe_fd);
+				exec_cmd(cmd_start, env, &infd, has_pipe, pipefd);
 				if (has_pipe)
-					fd_in = pipe_fd[0];
+					infd = pipefd[0];
 			}
 		}
 		if (!has_pipe)
-			fd_in = -1;
+			infd = -1;
 	}
 	while (waitpid(-1, &status, 0) != -1);
 	return (status);
