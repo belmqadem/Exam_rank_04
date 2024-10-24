93b538215b022818019e46a8cf5eafeaba3ee91e
valarmo <adilfront3@gmail.com>
2024-10-24 17:18:43 +0100

    exam 04

--- microshell.c
+++ microshell.c
@@ -1,109 +1,134 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 
-void	err(char *str)
-{
-	while (*str)
-		write (2, str++, 1);
+#define ERR_FATAL "error: fatal\n"
+#define ERR_CD_ARGS "error: cd: bad arguments\n"
+#define ERR_CD_FAIL "error: cd: cannot change directory to "
+#define ERR_EXECV "error: cannot execute "
 
+void err(char *str)
+{
+    while (*str)
+        write(2, str++, 1);
 }
 
-void	close_fds(int *fd)
+int cd(char **av, int i)
 {
-	if (fd[0] != -1) close(fd[0]);
-	if (fd[1] != -1) close(fd[1]);
-}
-
-int		cd(char **av)
-{
-	if (!av[1] || av[2])
+    if (i != 2)
 	{
-		err("error: cd: bad arguments\n");
-		return (1);
-	}
-	if (chdir(av[1]) == -1)
+        err(ERR_CD_ARGS);
+        return (1);
+    }
+    if (chdir(av[1]) == -1)
 	{
-		err("error: cd: cannot change directory to ");
+        err(ERR_CD_FAIL);
 		err(av[1]);
 		err("\n");
-		return (1);
-	}
-	return (0);
+        return (1);
+    }
+    return (0);
 }
 
-// This function handles the execution of a command and manages piping between processes.
-int		exec_cmd(char **av, char **env, int *infd, int has_pipe, int *pipefd)
+void set_pipe(int has_pipe, int *fd, int end)
 {
-	int pid, status;
-	if (has_pipe && pipe(pipefd) == -1)
+    if (has_pipe)
 	{
-		err("error: fatal\n");
-		exit (1);
-	}
-	if ((pid = fork()) == -1)
-	{
-		err("error: fatal\n");
-		exit(1);
-	}
-	if (pid == 0)
-	{
-		if (infd && *infd != -1 && dup2(*infd, 0) == -1)
+        if (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1)
 		{
-			err("error: fatal\n");
-			exit(1);
-		}
-		if (has_pipe && dup2(pipefd[1], 1) == -1)
-		{
-			err("error: fatal\n");
-			exit (1);
-		}	
-		close_fds(infd);
-		close_fds(pipefd);
-		execve(av[0], av, env);
-		err("error: cannot execute ");
-        err(av[0]);
-        err("\n");
+            err(ERR_FATAL);
+            exit(1);
+        }
+    }
+}
+
+int execute(char **av, int i, char **env, int *fd_in)
+{
+    int fd[2], pid, status;
+    int has_pipe = av[i] && !strcmp(av[i], "|");
+
+    if (!strcmp(*av, "cd"))
+        return cd(av, i);
+    if (has_pipe && pipe(fd) == -1)
+	{
+        err(ERR_FATAL);
         exit(1);
-	}
-	if (infd && *infd != -1) close (*infd);
-	if (has_pipe) close(pipefd[1]);
-	if (!has_pipe) waitpid(pid, &status, 0);
-	return (pid);
+    }
+    if ((pid = fork()) == -1)
+	{
+        err(ERR_FATAL);
+        exit(1);
+    }
+
+	// Child Process
+    if (pid == 0)
+	{
+		// Null-terminate the current command
+        av[i] = NULL;
+
+        // Only redirect stdin from a previous pipe if fd_in is valid
+        if (fd_in && *fd_in != -1)
+		{
+            if (dup2(*fd_in, 0) == -1)
+			{
+                err(ERR_FATAL);
+                exit(1);
+            }
+            close(*fd_in);
+        }
+
+        // If there is a pipe, redirect output to the write-end of the pipe
+        set_pipe(has_pipe, fd, 1);
+
+		// Execute the command
+        execve(*av, av, env);
+        err(ERR_EXECV), err(*av), err("\n");
+        exit(1);
+    }
+
+    // Parent process: wait for the child to finish
+    waitpid(pid, &status, 0);
+
+    // Close the write-end of the pipe
+    if (has_pipe)
+        close(fd[1]);
+
+    // If there was input from a previous pipe, close it
+    if (fd_in && *fd_in != -1)
+        close(*fd_in);
+
+    // Pass the read-end of the pipe to the next command
+    if (has_pipe)
+        *fd_in = fd[0];
+    else
+        *fd_in = -1;
+
+    return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
-int	main(int ac, char **av, char **env)
+int main(int ac, char **av, char **env)
 {
-	int i = 1, status = 0, has_pipe = 0, infd = -1, pipefd[2];
-	char **cmd_start = NULL;
-	(void)ac;
-	while (av[i])
+    (void)ac;
+    int i = 0, status = 0, fd_in = -1;
+
+    while (av[i])
 	{
-		cmd_start = &av[i];
-		while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
-			i++;
-		has_pipe = (av[i] && strcmp(av[i], "|") == 0);
-		if (av[i])
-			av[i++] = NULL;
-		if (cmd_start[0])
-		{
-			if (!strcmp(cmd_start[0], "cd"))
-			{
-				if (cd(cmd_start))
-					status = 1;
-			}
-			else
-			{
-				exec_cmd(cmd_start, env, &infd, has_pipe, pipefd);
-				if (has_pipe)
-					infd = pipefd[0];
-			}
-		}
-		if (!has_pipe)
-			infd = -1;
-	}
-	while (waitpid(-1, &status, 0) != -1);
-	return (status);
+        // Skip any semicolons
+        av += i + 1;
+        i = 0;
+
+        // Find the next `|` or `;`
+        while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+            i++;
+
+		// We found a command to execute
+        if (i > 0)
+            status = execute(av, i, env, &fd_in);
+
+        // If it's a semicolon, reset the pipe
+        if (av[i] && !strcmp(av[i], ";"))
+            fd_in = -1;
+    }
+    return status;
 }
